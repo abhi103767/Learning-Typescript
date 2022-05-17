8bb36622b0161aff4b88c2fa72fba59567a2fe7f
Avinash <avinash1234sharma1@gmail.com>
2022-05-17 23:44:27 +0530

    adding computer assignment

--- todos/src/components/Computer.tsx
+++ todos/src/components/Computer.tsx
@@ -1,9 +1,11 @@
 import { nanoid } from 'nanoid'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import  Typography from '@mui/material/Typography'
 import  TextField  from '@mui/material/TextField'
 import Box from '@mui/material/Box'
 import  Button  from '@mui/material/Button'
+import axios, {AxiosResponse} from 'axios'
+import List from './List'
 
 type FormData = {
     id : string,
@@ -32,22 +34,47 @@ function Computer() {
     });
 
 
-    const [list,setList] = useState([])
 
 
-    function handleChange(e:React.ChangeEvent){
-        console.log(e.target.nodeValue)
-    //     setFormData((perv) => {
-    //   return {
-    //       [e.target.name]
-    //   }
-    //     })
+    const [list,setList] = useState<FormData[]>([])
+
+    useEffect(() => {
+        getData()
+    })
+
+   console.log(formData)
+    function handleChange(e:React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>):void{
+      setFormData((perv) => {
+       return {
+           ...perv,
+           [e.target.name] : e.target.value
+       }   
+      });
+    
     }
+    function handleClick(e:React.MouseEvent<HTMLButtonElement>){
+  axios.post('http://localhost:8080/computers', formData).then(
+               (res) => {
+                   console.log(res.data)
+               }
+           )
+        //    console.log(list)
+        }
+
+        function getData(){
+            axios.get('http://localhost:8080/computers')
+            .then( (response : AxiosResponse<FormData[]>) => {
+                const   {data}  = response;
+               setList(data);
+                 }); 
+        }
+      
 
   return (
     <div>
-        <Box my={6} >
-        <Box p={2}> 
+        <Box width={'50%'} p={10} border="1px solid black" 
+        margin={'auto'} my={4} >
+        <Box  p={2} > 
         <TextField 
         type={'text'}
         variant='outlined'
@@ -64,7 +91,7 @@ function Computer() {
         
         <Box p={2}>
         <TextField
-        text={} 
+        type={'number'} 
         name='makeyear' 
         variant='outlined'
          size='small'
@@ -74,11 +101,12 @@ function Computer() {
             handleChange(e);
         }}/>
           </Box>
-          <Box>
-          <TextField 
+          <Box >
+          <TextField
+          type = 'text'
         variant='outlined'
          size='small'
-          placeholder='Enter the modal Number'
+          placeholder='Enter the operating system '
           name = 'operatingsystem'
           value={formData.operatingsystem}
           onChange={  (e) => {
@@ -87,6 +115,7 @@ function Computer() {
           </Box>
           <Box p={2}>
           <TextField 
+          type={'number'}
         variant='outlined'
          size='small'
           placeholder='Enter the screenheight'
@@ -98,6 +127,7 @@ function Computer() {
           </Box>
           <Box p={2}>
           <TextField 
+          type={'number'}
         variant='outlined'
          size='small'
          name='screenwidth'
@@ -109,6 +139,7 @@ function Computer() {
           </Box>
           <Box p={2}>
           <TextField 
+          type={'number'}
         variant='outlined'
          size='small'
           placeholder='Enter the Price'
@@ -118,7 +149,16 @@ function Computer() {
             handleChange(e);
         }}/>
           </Box>
-          <Button variant='outlined'>Save Data</Button>
+          <Button  
+           variant='outlined' onClick={handleClick}>Save Data</Button>
+      </Box>
+
+      <Box>
+          {
+             list.map((item) => {
+              return   <List formData={formData}/>
+             }) 
+          }
       </Box>
           
     </div>
